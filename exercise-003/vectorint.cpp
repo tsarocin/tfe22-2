e5b419f60190c003cb5e4dc3880fda423630c983
Christian Ege <ch@ege.io>
2023-10-30 16:08:42 +0100

    feat: add ctor and dtor

--- exercise-003/vectorint.cpp
+++ exercise-003/vectorint.cpp
@@ -1,9 +1,12 @@
 #include "vectorint.hpp"
 
-VectorInt::VectorInt(int) {
-
+VectorInt::VectorInt(int size) {
+    mp_Data = new int[size];
 }
 
 VectorInt::~VectorInt() {
-    
+    if(mp_Data != nullptr) {
+        delete [] mp_Data;
+    }
+    mp_Data = nullptr;
 }
