57c845a1f3938f0bb25360c832c3cfcf477751f1
Christian Ege <ch@ege.io>
2023-11-07 08:41:01 +0100

    feat: add skeleton functions
    Signed-off-by: Christian Ege <ch@ege.io>

--- exercise-003/vectorint.cpp
+++ exercise-003/vectorint.cpp
@@ -2,6 +2,7 @@
 
 VectorInt::VectorInt(int size) {
     mp_Data = new int[size];
+    m_size = size;
 }
 
 VectorInt::~VectorInt() {
@@ -9,4 +10,23 @@ VectorInt::~VectorInt() {
         delete [] mp_Data;
     }
     mp_Data = nullptr;
+}
+
+int& VectorInt::at( size_t pos ){
+    if(pos < m_size) {
+        return mp_Data[m_size];
+    }
+    // better use an exception!
+    return m_null;
+}
+
+size_t VectorInt::size() const {
+    return m_size;
+}
+
+void VectorInt::resize( size_t count ) {
+
+}
+void VectorInt::push_back( const int& value ) {
+
 }
