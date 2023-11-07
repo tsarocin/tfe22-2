02ffae9e375d048faadd4206c99b62af87f1d2b9
Christian Ege <ch@ege.io>
2023-11-07 10:05:57 +0100

    feat: fill the vector
    Signed-off-by: Christian Ege <ch@ege.io>

--- exercise-003/vectorint.cpp
+++ exercise-003/vectorint.cpp
@@ -5,6 +5,9 @@
 VectorInt::VectorInt(int size) {
     mp_Data = new int[size];
     m_size = size;
+    for(int i = 0; i<m_size; i++) {
+        mp_Data[i] = m_null;
+    }
 }
 
 VectorInt::~VectorInt() {
